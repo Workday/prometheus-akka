46b55f36f9542f717dfa7b1c46eee4b91c2ba8b9
pj.fanning <pj.fanning@workday.com>
2018-02-01 17:50:54 +0000

    license tidy up

--- src/test/scala/akka/monitor/instrumentation/EnvelopeSpec.scala
+++ src/test/scala/akka/monitor/instrumentation/EnvelopeSpec.scala
@@ -1,5 +1,6 @@
 /*
  * =========================================================================================
+ * Copyright © 2017,2018 Workday, Inc.
  * Copyright © 2017 the kamon project <http://kamon.io/>
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
